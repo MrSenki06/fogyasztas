1ee694837b85e30c40eab4235d002041d6907af2
Hbence10 <93594678+Hbence10@users.noreply.github.com>
2022-03-31 14:24:01 +0200

    probaly js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function szamol() {
 let F = document.getElementById('fogyasztás').value;
 let V = document.getElementById('tank').value;
 let UT = document.getElementById('ut').value;
+
+let megtehetoUT = V * 100 / F;
+
+document.getElementById('jel').innerHTML = "Az út megtehető tankolás nélkül"
+
+if (megtehetoUT < UT) {
+    document.getElementById('megoldas').innerHTML = "Az út során tankolni kell!";
+} else {
+    document.getElementById('megoldas').innerHTML = "Az út megtehető tankolás nélkül"
+}
+}
