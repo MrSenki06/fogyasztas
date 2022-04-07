3aa4568a70ea168665a1b769667a5ee4a16af18d
MrSenki06 <93594938+MrSenki06@users.noreply.github.com>
2022-04-07 14:23:39 +0200

    mogoldas1

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+
 let szamolas = function szamol () {}
+
 function szamol () {
     let F = document.getElementById('fogyasztas').value;
     let V = document.getElementById('tank').value;
@@ -12,5 +14,6 @@ function szamol () {
     } else {
     document.getElementById('megoldas').innerHTML = "Az út megtehető tankolás nélkül."
     }
+    return false;
 }
 document.getElementById('szamol').addEventListener('click', szamolas);
